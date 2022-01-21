374ad01562254f8bf418f30b6b1fcac8727eaed0
ben <kakengi_bernard@hotmail.com>
2022-01-21 17:07:14 +0200

    Finished implementing Modal

--- react-frontend/src/places/components/PlaceItem.jsx
+++ react-frontend/src/places/components/PlaceItem.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../../shared/components/Card';
+import Button from '../../shared/components/FormElements/Button';
+import Modal from '../../shared/components/UIElements/Modal';
 import './PlaceItem.css';
 
 const PlaceItem = ({
@@ -11,26 +13,47 @@ const PlaceItem = ({
 	address,
 	creatorId,
 }) => {
+	const [showMap, setShowMap] = useState(false);
+
+	const handleOpenMap = () => setShowMap(true);
+
+	const handleCloseMap = () => setShowMap(false);
 	return (
-		<li className="place-item">
-			<Card className="place-item__content">
-				<div className="place-item__image">
-					<img src={image} alt={title} />
+		<>
+			<Modal
+				show={showMap}
+				onCancel={handleCloseMap}
+				header={address}
+				contentClass="place-item__modal-content"
+				footerClass="place-item__modal-actions"
+				footer={<Button onClick={handleCloseMap}>CLOSE</Button>}
+			>
+				<div className="map-container">
+					<h2>THE MAP</h2>
 				</div>
+			</Modal>
+			<li className="place-item">
+				<Card className="place-item__content">
+					<div className="place-item__image">
+						<img src={image} alt={title} />
+					</div>
 
-				<div className="place-item__info">
-					<h2>{title}</h2>
-					<h3>{address}</h3>
-					<p>{description}</p>
-				</div>
+					<div className="place-item__info">
+						<h2>{title}</h2>
+						<h3>{address}</h3>
+						<p>{description}</p>
+					</div>
 
-				<div className="place-item__actions ">
-					<button>VIEW ON MAP</button>
-					<button>EDIT</button>
-					<button>DELETE</button>
-				</div>
-			</Card>
-		</li>
+					<div className="place-item__actions ">
+						<Button inverse onClick={handleOpenMap}>
+							VIEW ON MAP
+						</Button>
+						<Button to={`/places/${id}`}>EDIT</Button>
+						<Button danger>DELETE</Button>
+					</div>
+				</Card>
+			</li>
+		</>
 	);
 };
 
